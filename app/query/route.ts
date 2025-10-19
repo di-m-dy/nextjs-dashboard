2bccd61761ed1f77317101787014f417ef0a00e8
di-m-dy <dimdy11.11@gmail.com>
2025-10-20 01:38:00 +0200

    9 chapter end

--- app/query/route.ts
+++ app/query/route.ts
@@ -1,7 +1,7 @@
 // import postgres from 'postgres';
-
+//
 // const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
-
+//
 // async function listInvoices() {
 // 	const data = await sql`
 //     SELECT invoices.amount, customers.name
@@ -9,7 +9,7 @@
 //     JOIN customers ON invoices.customer_id = customers.id
 //     WHERE invoices.amount = 666;
 //   `;
-
+//
 // 	return data;
 // }
 
